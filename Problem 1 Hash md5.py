4852e562270a3bd581c7d83784b68f72fab1f527
Omkar Bhusnale <73767886+DarkSoul231@users.noreply.github.com>
2021-07-04 01:36:35 +0530

    Update Problem 1 Hash md5.py

--- Problem 1 Hash md5.py
+++ Problem 1 Hash md5.py
@@ -1,5 +1,6 @@
 import hashlib
 
+#Used to check weather how many algoriths present in hashlib library
 #print(hashlib.algorithms_available)
 
 print("-----------------------------------------------")
@@ -7,8 +8,10 @@ print("<>>>>>>>>>><MD5 Hashing><<<<<<<<<<>")
 print("-----------------------------------------------")
 string = input("Enter a String: ")
 
+#Storing Hash Value in Object...
 hash_object = hashlib.md5(str(string).encode('utf-8'))
 
+#hexdigest() function to convert Raw Hash to HexaDecimal Format..
 print(f'Hash of || {string} || is : ', hash_object.hexdigest())
 print("-----------------------------------------------")
 
